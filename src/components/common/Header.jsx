c7ea779c9d325b444fbafb6b36f6bfc23e39dd35
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-01-23 20:30:26 +0200

    830

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,3 +1,49 @@
+import Link from 'next/link';
+import { CgMenu } from 'react-icons/cg';
+
 export const Header = () => {
-  return 'header';
+  const jsxVar = <span class="bg-white block w-4 h-4 rounded-full"></span>;
+
+  return (
+    <>
+      <section className="flex lg:flex-col justify-between items-center h-full lg:py-4">
+        <Link href="/">
+          {/* no use for anchor */}
+          {jsxVar}
+        </Link>
+
+        <button
+          title="Menu"
+          type="button"
+          onClick={() => {
+            alert('open menu');
+          }}
+        >
+          <CgMenu size="32"></CgMenu>
+        </button>
+
+        <ul className="hidden lg:block">
+          <li>{jsxVar}</li>
+        </ul>
+      </section>
+
+      <nav
+        className={`absolute left-0 -top-full transition-transform transform-gpu w-screen h-screen bg-neutral-900`}
+      >
+        <ul>
+          <li>
+            <Link href="/" title="Home">
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
