3273e202bb3056649bd30c33cadb25251ab2d1f0
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-01-25 21:36:12 +0200

    936

--- src/components/catalog/ProductGrid.jsx
+++ src/components/catalog/ProductGrid.jsx
@@ -1,11 +1,15 @@
 import { baseUrl } from '@/index';
+import { css } from '@emotion/css';
 import { useEffect, useState } from 'react';
 import { ProductTile } from '.';
 
-export const ProductGrid = () => {
+export const ProductGrid = ({ perRow = '4/row' }) => {
   const [products, setProducts] = useState([]);
 
-  console.log('rerender');
+  // useMemo
+  const itemsPerRow = parseInt(perRow);
+
+  console.log('render product grid');
 
   useEffect(() => {
     fetch(`${baseUrl}/products`)
@@ -21,8 +25,18 @@ export const ProductGrid = () => {
     return <>There are no products</>;
   }
 
+  const gridCss = css`
+    display: grid;
+    row-gap: 32px;
+
+    @media (min-width: 1024px) {
+      grid-template-columns: repeat(${itemsPerRow}, 1fr);
+      column-gap: 32px;
+    }
+  `;
+
   return (
-    <ul>
+    <ul className={gridCss}>
       {products.map((product) => {
         return (
           <li key={product.id}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,13 @@
 import { GridControls, ProductGrid } from '@/components/catalog';
 import { Layout } from '@/layouts';
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
+  const [perRow, setItemsPerRow] = useState('4/row');
+
+  console.log('render home');
+
   return (
     <>
       <Head>
@@ -14,11 +19,11 @@ export default function Home() {
       <Layout>
         <main className="container px-4 mx-auto">
           <header className="flex justify-end">
-            <GridControls></GridControls>
+            <GridControls set={setItemsPerRow}></GridControls>
           </header>
 
-          <section>
-            <ProductGrid></ProductGrid>
+          <section className="mt-16">
+            <ProductGrid perRow={perRow}></ProductGrid>
           </section>
         </main>
       </Layout>
