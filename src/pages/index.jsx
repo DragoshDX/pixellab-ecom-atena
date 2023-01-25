c761b64ccfb9e71a657e67209c3db3b243147c4f
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-01-25 20:20:40 +0200

    820

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,21 @@
+import { ProductGrid } from '@/components/catalog/ProductGrid';
 import { Layout } from '@/layouts';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setProducts(data);
-      });
-  }, [setProducts]);
-
   return (
     <>
       <Head>
-        <title>Pixellab Ecom App</title>
+        <title>Home - Pixellab Ecom App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Layout>
-        <main>
-          {products.map((product) => {
-            return <span key={product.id}>{product.title}</span>;
-          })}
+        <main className="container px-4 mx-auto">
+          <section>
+            <ProductGrid></ProductGrid>
+          </section>
         </main>
       </Layout>
     </>
