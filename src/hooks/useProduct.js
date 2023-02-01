7241763bf68cb685511bedd0462bd816c7d2a7b5
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-02-01 20:01:53 +0200

    801

--- src/hooks/useProduct.js
+++ src/hooks/useProduct.js
@@ -1,20 +1,36 @@
 import { useEffect, useState } from 'react';
+import { baseUrl } from '..';
+
+const productCache = {};
 
 export const useProduct = (productId) => {
   const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [httpStatus, setHttpStatus] = useState(200);
 
   useEffect(() => {
     if (productId) {
-      fetch(`${baseUrl}/products/${productId}`)
-        .then((response) => {
-          // .json returns promise
-          return response.json();
-        })
-        .then((product) => {
-          setProduct(product);
-        });
+      if (productCache[productId] === undefined) {
+        fetch(`${baseUrl}/products/${productId}`)
+          .then((response) => {
+            // .json returns promise
+            return response.json();
+          })
+          .then((product) => {
+            setProduct(product);
+            productCache[productId] = product;
+          })
+          .catch(() => {
+            setHttpStatus(404);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      } else {
+        setProduct(productCache[productId]);
+      }
     }
   }, [setProduct, productId]);
 
-  return { product };
+  return { product, httpStatus, loading };
 };
