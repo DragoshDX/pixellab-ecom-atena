c50c4426ae767050a04fbf7502d26d3d2302d852
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-02-13 21:51:29 +0200

    addtocart

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -6,10 +6,12 @@ export const cartContext = createContext();
 const x = cartContext;
 
 export const CartContext = ({ children }) => {
-  const { cartProducts, loading, cartId } = useCart(2);
+  const { cartProducts, loading, cartId, userId, setCartProducts } = useCart(2);
 
   return (
-    <x.Provider value={{ cartProducts, loading, cartId }}>
+    <x.Provider
+      value={{ cartProducts, loading, cartId, userId, setCartProducts }}
+    >
       {children}
     </x.Provider>
   );
